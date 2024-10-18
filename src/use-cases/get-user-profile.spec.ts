e44f0877381c41125b741d26b9f2068f8ee41e64
unknown <nbarretoduarte@gmail.com>
2024-10-18 14:31:02 -0300

    Caso de uso de perfil

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-user-repository";
+import { hash } from "bcryptjs";
+import { GetUserProfileUseCase } from "./get-user-profile";
+import { ResourceNotFoundError } from "./errors/resource-noy-found-error";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfileUseCase;
+
+describe("Get User Profile Use Case", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line new-cap
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(usersRepository);
+  });
+
+  it("Should be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      name: "Jonh Doe",
+      email: "jonhdoe@gmail.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.name).toEqual("Jonh Doe");
+  });
+
+  it("Should not be able to get user profile with wrong id", async () => {
+    expect(() =>
+      sut.execute({
+        userId: "non-existing-id",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
