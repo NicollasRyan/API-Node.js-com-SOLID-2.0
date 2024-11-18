ba96c492b58c482a53cd8a0a30c23b2a7ae9d849
unknown <nbarretoduarte@gmail.com>
2024-11-18 12:07:49 -0300

    Controller de perfil

--- src/http/Routes.ts
+++ src/http/Routes.ts
@@ -1,10 +1,11 @@
-import { FastifyInstance } from "fastify";
-import { register } from "./Controllers/register";
-import { authenticate } from "./Controllers/authenticate";
-import { profile } from "./Controllers/profile";
+import { FastifyInstance } from 'fastify'
+import { register } from './Controllers/register'
+import { authenticate } from './Controllers/authenticate'
+import { profile } from './Controllers/profile'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/users", register);
-  app.post("/sessions", authenticate);
-  app.get("/me", profile)
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
